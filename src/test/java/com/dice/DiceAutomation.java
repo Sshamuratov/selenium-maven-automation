fe9e2514f506312fe91f7ff49ecc35cb8cc81064
Sshamuratov <bzikbzik775@gmail.com>
2018-06-13 18:28:13 -0400

    add dice automation

---
+++ src/test/java/com/dice/DiceAutomation.java
@@ -0,0 +1,71 @@
+package com.dice;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.management.RuntimeErrorException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DiceAutomation {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		String url = "https://dice.com";
+		driver.get(url);
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Job Search for Technology Professionals | Dice.com";
+
+		if (actualTitle.equals(expectedTitle)) {
+			System.out.println("Step PASS. Dice homepage successfully loaded");
+
+		} else {
+			System.out.println("Step FAIL. Dice homepage did not load successfully");
+			throw new RuntimeException("Step FAIL. Dice homepage did not load successfully");
+		}
+
+		String keyword = "java developer";
+
+		driver.findElement(By.id("search-field-keyword")).sendKeys(keyword);
+
+		String location = "22102";
+
+		driver.findElement(By.id("search-field-location")).clear();
+		driver.findElement(By.id("search-field-location")).sendKeys(location);
+		driver.findElement(By.id("findTechJobs")).click();
+
+		String count = driver.findElement(By.id("posiCountId")).getText();
+
+		System.out.println(count);
+
+		double countResult = Integer.parseInt(count.replaceAll(",", ""));
+
+		if (countResult > 0) {
+			System.out.println("Step PASS: Keyword: " + keyword + "search returned " + 
+		countResult + " results in " + location);
+
+		} else {
+			System.out.println("Step FAIL: Keyword: " + keyword + "search returned " + 
+					countResult + " results in " + location);
+		}
+		
+		
+		driver.close();
+		
+	}
+	
+	
+
+}
